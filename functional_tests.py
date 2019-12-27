05e19ce05b0bd66f683053fb7e2434d930088e85
Nicholas Henry <nicholas.henry@gmail.com>
2019-12-27 14:02:08 -0500

    Update functional test for to-do list

--- functional_tests.py
+++ functional_tests.py
@@ -1,10 +1,25 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+import unittest
 
-options = Options()
-options.headless = True
+class NewVisitor(unittest.TestCase):
 
-browser = webdriver.Firefox(options=options)
-browser.get('http://localhost:8000')
+    def setUp(self):
+        options = Options()
+        options.headless = True
+        self.browser = webdriver.Firefox(options=options)
 
-assert 'Django' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do-app. She goes
+        # check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main()
